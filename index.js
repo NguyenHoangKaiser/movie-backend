f18834c3296b447de67bb01e149922891da6aa88
HuyHoang <dragonhuyhoang2001@gmail.com>
2022-10-04 22:38:48 +0700

    Connect Movies Route

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+//@ts-nocheck
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
 async function main() {
   dotenv.config();
-  const uri =
-    "mongodb+srv://admin:1DQgkHSIkOEaEJwn@newcluster.srgg8lr.mongodb.net/?retryWrites=true&w=majority";
-
-  const client = new mongodb.MongoClient(uri);
-  const port = process.env.PORT || 8000;
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+  const port = 8000;
   try {
     // Connect to the MongoDB cluster
     await client.connect();
 
     // Make the appropriate DB calls
-    await listDatabases(client);
+    // await listDatabases(client);
     
     app.listen(port, () => {
       console.log("server is running on port:" + port);
@@ -25,9 +23,9 @@ async function main() {
 }
 main().catch(console.error);
 
-async function listDatabases(client){
-    let databasesList = await client.db().admin().listDatabases();
+// async function listDatabases(client){
+//     let databasesList = await client.db().admin().listDatabases();
  
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };

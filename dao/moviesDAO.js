a07f03498cd982913e4004b0d8d1fc708eb00715
HuyHoang <dragonhuyhoang2001@gmail.com>
2022-10-04 23:18:15 +0700

    Create the Movies Data Access Object

---
+++ dao/moviesDAO.js
@@ -0,0 +1,61 @@
+let movies;
+
+export default class MoviesDAO {
+  static async injectDB(conn) {
+    if (movies) {
+      return;
+    }
+    try {
+      movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
+    } catch (e) {
+      console.error(
+        `Unable to establish a collection handle in moviesDAO: ${e}`
+      );
+    }
+  }
+
+  //async method to get all movies
+  static async getMovies({
+    filters = null,
+    page = 0,
+    moviesPerPage = 20,
+  } = {}) {
+    let query;
+    
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      }
+    }
+
+    let cursor;
+
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
+    }
+
+    const displayCursor = cursor
+      .limit(moviesPerPage)
+      .skip(moviesPerPage * page);
+
+    try {
+      const moviesList = await displayCursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
+
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(
+        `Unable to convert cursor to array or problem counting documents, ${e}`
+      );
+      return { moviesList: [], totalNumMovies: 0 };
+    }
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import MoviesDAO from "./dao/moviesDAO.js";
+
 async function main() {
   dotenv.config();
   const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
@@ -9,9 +11,7 @@ async function main() {
   try {
     // Connect to the MongoDB cluster
     await client.connect();
-
-    // Make the appropriate DB calls
-    // await listDatabases(client);
+    await MoviesDAO.injectDB(client);
     
     app.listen(port, () => {
       console.log("server is running on port:" + port);
@@ -21,11 +21,7 @@ async function main() {
     process.exit(1);
   }
 }
+
 main().catch(console.error);
 
-// async function listDatabases(client){
-//     let databasesList = await client.db().admin().listDatabases();
- 
-//     console.log("Databases:");
-//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
+
